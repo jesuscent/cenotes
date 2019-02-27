7fac39f1e449cbb8bdf370afe1b33a886583a5cd
diegoar1 <33189051+diegoar1@users.noreply.github.com>
2019-02-27 09:49:48 -0500

    mejora del header

--- src/app/componentes/rutas/rutas.component.ts
+++ src/app/componentes/rutas/rutas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/servicios/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-rutas',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RutasComponent implements OnInit {
 
-  constructor() { }
+  isLoggedIn: boolean;
+  loggedInUser: string;
+  permitirRegistro: boolean;
+
+  constructor(
+    private loginService: LoginService,
+    private router: Router,
+    
+  ) { }
 
   ngOnInit() {
+    this.loginService.getAuth().subscribe( auth => {
+      if(auth){
+        this.isLoggedIn = true;
+        this.loggedInUser = auth.email;
+      }
+      else{
+        this.isLoggedIn = false;
+      }
+    });
+
+  
   }
+
+  logout(){
+    this.loginService.logout();
+    this.isLoggedIn = false;
+    this.router.navigate(['/login']);
+  }
+
 
 }
