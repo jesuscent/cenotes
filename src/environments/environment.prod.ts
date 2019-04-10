9283822ec54f742bac55849e6066daffff08bdfd
jesuscent <44858038+jesuscent@users.noreply.github.com>
2019-04-10 18:48:50 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,5 @@
 export const environment = {
   production: true,
-  firestore:{
-    apiKey: "AIzaSyB1qSdcbtStsoaiA0bO1lFI2H2fr_CFkcw",
-    authDomain: "cenotes-5e387.firebaseapp.com",
-    databaseURL: "https://cenotes-5e387.firebaseio.com",
-    projectId: "cenotes-5e387",
-    storageBucket: "cenotes-5e387.appspot.com",
-    messagingSenderId: "486616394822"
+  
   }
 };
